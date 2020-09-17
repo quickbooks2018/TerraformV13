3b25b24c8c8f692f8ab3389b529cb2d091649f44
Your Name <you@example.com>
2020-09-17 10:46:49 +0500

    EC2 userdata updated with apachebench installation for load testing

--- vpc-ecs-rds-alb/modules/aws-ec2/user-data/user-data.sh
+++ vpc-ecs-rds-alb/modules/aws-ec2/user-data/user-data.sh
@@ -1,19 +1,7 @@
 #!/bin/bash
-# Purpose: Vault Dynamic Credentials Usage in actual production
+# Purpose: ApacheBench Installation of load testing on ALB with auto-scaling enabled
 # Maintainer: DevOps Muhammad Asim <quickbooks2018@gmail.com>
 
-# Docker Installation
+# ApacheBench Installation
 
-curl -fsSL https://get.docker.com -o get-docker.sh
-sh get-docker.sh      2>&1 > /dev/null
-rm -rf get-docker.sh
-yum install -y docker 2>&1 > /dev/null
-systemctl start docker
-systemctl enable docker
-
-# Docker Compose Installation
-
-curl -L "https://github.com/docker/compose/releases/download/1.25.5/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
-chmod +x /usr/local/bin/docker-compose
-ln -s /usr/local/bin/docker-compose /usr/bin/docker-compose
-docker-compose --version
+yum install -y httpd24-tools
